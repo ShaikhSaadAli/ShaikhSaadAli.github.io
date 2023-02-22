258abf550759edbce00cb90a40cf92a06e36ea80
Saad <sk.saad3621@gmai.com>
2023-02-22 20:30:54 +0530

    Adding all Files

---
+++ www.classcentral.com/in.getclicky.com/in (2).php
@@ -0,0 +1,4 @@
+// (☞ﾟ∀ﾟ)☞ static31
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '1426610119');
+}
---
+++ www.classcentral.com/in.getclicky.com/in (4).php
@@ -0,0 +1,4 @@
+// (☞ﾟ∀ﾟ)☞ static32
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '1426610119');
+}
